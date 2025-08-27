5bbeb2c1edb83671606074b4dd25a02538cc2f17
microsoftfeed-maker <microsoftfeed@gmail.com>
2025-08-27 14:42:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-app/',   // 👈 IMPORTANT: use your repo name here
+  base: '/my-app/'
 })
